bd15dbf5e2c602944858753bbc9509b6338d4de7
komasamo <komasamo1@gmail.com>
2026-06-03 17:30:56 +0000

    Set up basic database configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.room3.common)
+    implementation(libs.androidx.room3.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/com/kardify/app/db/RoomDatabase.kt
+++ app/src/main/java/com/kardify/app/db/RoomDatabase.kt
@@ -1,3 +1,64 @@
 package com.kardify.app.db
 
+import android.content.Context
+import android.provider.ContactsContract
+import androidx.lifecycle.LiveData
+import androidx.room3.ColumnInfo
+import androidx.room3.Dao
+import androidx.room3.Database
+import androidx.room3.Delete
+import androidx.room3.Entity
+import androidx.room3.Insert
+import androidx.room3.OnConflictStrategy
+import androidx.room3.PrimaryKey
+import androidx.room3.Query
+import androidx.room3.Room
+import androidx.room3.RoomDatabase
+import androidx.room3.Update
+import kotlinx.coroutines.flow.Flow
+
 //samo
+@Entity(tableName = "questions")
+data class Question(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "front_side") val frontSide: String?,
+    @ColumnInfo(name = "back_side") val backSide: String?
+)
+
+
+@Dao
+interface QuestionDao {
+    @Insert
+    suspend fun insert(question: Question)
+
+    @Update
+    suspend fun update(question: Question)
+
+    @Delete
+    suspend fun delete(question: Question)
+
+    @Query("SELECT * FROM questions")
+    fun getAllQuestions(): Flow<List<Question>>
+}
+@Database(entities = [Question::class], version = 1)
+abstract class QuestionDatabase : RoomDatabase() {
+
+    abstract fun questionDao(): QuestionDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: QuestionDatabase? = null
+
+        fun getDatabase(context: Context): QuestionDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    QuestionDatabase::class.java,
+                    "questions_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
